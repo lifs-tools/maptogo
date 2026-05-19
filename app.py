c7f791639876a51d6343f63f95fa9e9599a1535a
Dominik Kopczynski <dominik.kopczynski@tu-dortmund.de>
2026-05-19 12:55:34 +0200

    update

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ server = app.server
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(host = "0.0.0.0", debug = True, port = 8050)
+    app.run_server(host = "0.0.0.0", debug = True, port = 8040)
